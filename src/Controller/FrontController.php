0a6f0e3020d07883fc4720fbfa9c39f6224e0789
teebbstudios <qww.zone@gmail.com>
2021-01-23 16:59:54 +0800

    11

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Teebb\CoreBundle\Entity\Content;
+use Teebb\CoreBundle\Repository\BaseContentRepository;
 
 class FrontController extends AbstractController
 {
@@ -13,8 +18,35 @@ class FrontController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+        return $this->render('front/index.html.twig');
+    }
+
+    /**
+     * @Route("/search", name="search")
+     */
+    public function search(Request $request, EntityManagerInterface $em): Response
+    {
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 10);
+
+        $title = $request->get('title', '');
+
+        /**@var BaseContentRepository $baseContentRepo * */
+        $baseContentRepo = $em->getRepository(Content::class);
+
+        $criteria = [];
+        if ($title !== '') {
+            $criteria = ['title' => '%' . $title . '%'];
+        }
+
+        /**@var Pagerfanta $paginator**/
+        $paginator = $baseContentRepo->createPaginator($criteria, ['id' => 'DESC']);
+        $paginator->setMaxPerPage($limit);
+        $paginator->setCurrentPage($page);
+
+        return $this->render('front/search_results.html.twig', [
+            'keyword' => $title,
+            'paginator' => $paginator
         ]);
     }
 }
